da5b32a6d3da6f708dcae970f106d69e229bf158
Sergey Kruglov <sk@zam.io>
2018-12-04 17:37:50 +0300

    [WIP] Added variables for versioning

--- cmd/customer-api.go
+++ cmd/customer-api.go
@@ -6,6 +6,14 @@ import (
 	"git.zam.io/microservices/customer-api/service"
 )
 
+var (
+	commitSHA   = "undefined"
+	commitRef   = "undefined"
+	commitRep   = "undefined"
+	commitEnv   = "undefined"
+	commitPipID = "undefined"
+)
+
 func main() {
 	app, err := service.New()
 	if err != nil {
